d4b176f914a40094f8b96f655502a4d34abe2c7a
Junior Forneiro <jose.forneiro@fatec.sp.gov.br>
2021-03-07 16:39:51 -0300

    Manipulação de String em C# 1

--- string1/Program.cs
+++ string1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace string1
 {
@@ -94,8 +95,79 @@ namespace string1
 
             // *******************************************************************************************************
 
+            // Indices
 
+            Console.WriteLine("\ne) Indices:\n");
 
+            var texto1 = "Este texto é um teste";
+
+            Console.WriteLine(texto1.IndexOf("é")); // O IndexOF sempre vai pedir um objeto do mesmo tipo. Eu passo o caractere que eu quero buscar e irá retornar a posição que está esse caractere. Só retorna o primeiro caractere que encontrar.
+            Console.WriteLine(texto1.LastIndexOf("um")); // O LastIndexOF sempre vai pedir um objeto do mesmo tipo. Eu passo o caractere que eu quero buscar e irá retornar o último indice que encontrar de uma letra ou de uma palavra.
+
+            // *******************************************************************************************************
+
+            // Métodos adicionais
+
+            Console.WriteLine("\nf) Métodos adicionais:\n");
+
+            texto1 = "Este texto é um teste";
+
+            Console.WriteLine(texto1.ToUpper()); // Converte todo o texto para maiusculo
+
+            Console.WriteLine(texto1.ToLower()); // Converte todo o texto para minusculo
+
+            Console.WriteLine(texto1.Insert(5, "AQUI ")); // Insere na posição 5 o texto AQUI
+
+            Console.WriteLine(texto1.Remove(5, 6)); // Remove a partir da posição 5 6 caracteres
+
+            Console.WriteLine(texto1.Length); // O Length trás a quantidade de caracteres que tem uma string.
+
+            // *******************************************************************************************************
+
+            // Manipulando Strings
+
+            Console.WriteLine("\ng) Manipulando Strings:\n");
+
+            texto1 = "Este texto é um teste";
+
+            Console.WriteLine(texto1.Replace("Este", "Isto")); // Replace("Este", "Isto") => Irá trocar a palavra Este pela palavra Isto
+
+            Console.WriteLine(texto1.Replace("e", "X")); // Replace("e", "X") => Irá trocar todos as letras e minusculas pela letra X maiuscula
+
+            var divisao = texto1.Split(" "); // Split(" ") => Irá quebrar o texto1 onde tem espaço. Como resultado teremos uma lista de strings.
+
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+            Console.WriteLine(divisao[3]);
+            Console.WriteLine(divisao[4]);
+
+            var resultado = texto1.Substring(5, 7); // Substring(5, 7) => Irá pegar a partir da posição 5 7 caracteres
+            Console.WriteLine(resultado);
+
+            resultado = texto1.Substring(5, texto1.LastIndexOf("o")); // Substring(5, texto1.LastIndexOf("o")) => Irá pegar a partir da posição 5 até o indice da última letra o. Ou seja o índice da última letra o é 9 então ele irá pegar 9 caracteres a partir do caractere 5. Não esquecer que começa a contar de 0 e não 1.
+            Console.WriteLine(resultado);
+
+            var texto2 = " Testando o Trim ";
+            Console.WriteLine(texto2.Trim()); // O Trim remove os espaços do início e do fim de uma string. Não remove os espaços do meio da string
+
+            Console.WriteLine(texto2.Replace(" ", "")); // Podemos tirar os espaços de todo o texto desse jeito.
+
+            // *******************************************************************************************************
+
+            // String Builder
+
+            Console.WriteLine("\nh) String Builder:\n");
+
+            texto1 += " aqui"; // Concatenação de string. texto1 += " aqui" => texto1 = texto1 + " aqui"
+
+            var texto3 = new StringBuilder(); // Esse é um método construtor. Build em inglês é contruir então sepre que ver a palavra Builder é um construtor. Esse método está dentro do using System.Text;
+            texto3.Append("Este texto é um teste");
+            texto3.Append(" é um teste");
+            texto3.Append(" Este texto");
+            texto3.Append(" Este é um teste");
+
+            Console.WriteLine(texto3); // O Console.WriteLine já converte para string pois caso contrario teramos que fazer um texto3.ToString(); pois texto3 é um StringBuilder e não String
         }
     }
 }
